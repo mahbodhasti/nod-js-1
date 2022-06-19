8655faa155b4da366e337cab1eb304ffa09ff582
mahbod hasti <mahbod.hasti@live.com>
2022-06-19 12:50:26 +0430

    refactor

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const {body,validationResult} = require('express-validator');
+
 const { default: helmet } = require('helmet');
-let users = require('./users')
+
 const morgan = require('morgan')
 const app = express();
+const config = require('config')
+const debug = require('debug')('app:main')
+const userRouter = require('./routes/users')
+const homeRouter = require('./routes/home')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.use(helmet())
-
+app.set('view engine', 'ejs');
+app.set('views','./views')
+// using for config package
+// console.log("Application name",config.get("name"))
+// console.log("version",config.get("version"))
+// console.log("SMS",config.get("SMS"))
+// console.log("SMS key",config.get("SMS.key")) kar nemikone
+console.log(process.env.NODE_ENV)
 if(app.get('env') == 'development'){
     console.log('morgan is active')
     app.use(morgan('tiny'))
@@ -19,96 +30,11 @@ if(app.get('env') == 'development'){
 console.log('NODE_ENV:',process.env.NODE_ENV)
 console.log(app.get('env'))
 
-app.get('/api/users',(req,res)=>{
-res.json({
-    user:users,
-    message:'ok'
-})
-})
-app.get('/api/users/:id',(req,res)=>{
-    const user = users.find( u => u.id === parseInt(req.params.id) )
-    if(!user) return res.status(404).json({user:'not find',message:'the user with the given id was not found'})
-res.json({
-    user:user,
-    message:'ok'
-})
-})
-app.post('/api/users',[
-    body('email','must be valid').isEmail(),
-    body('first_name','first name cant be empty').notEmpty(),
-    body('last_name','last name cant be empty').notEmpty(),
-],(req,res)=>{
-   const errors = validationResult(req);
-   if (!errors.isEmpty()){
-    return res.status(400).json({data:null,errors:errors.array(),message:'validation error'})
-   }
-users.push({id :users.length + 1,...req.body})
-res.json({
-    data : users,
-    message:'ok'
-})
-})
-app.post('/api/users',[
-    body('email','must be valid').isEmail(),
-    body('firs_name','first name cant be empty').notEmpty(),
-    body('last_name','first name cant be empty').notEmpty(),
-],(req,res)=>{
-   const errors = validationResult(req);
-   if (!errors.isEmpty()){
-    return res.status(400).json({data:null,errors:errors.array(),message:'validation error'})
-   }
-users.push({id :users.length + 1,...req.body})
-res.json({
-    data : users,
-    message:'ok'
-})
-})
 
-app.put('/api/users/:id',[
-    body('email','must be valid').isEmail(),
-    body('first_name','first name cant be empty').notEmpty(),
-    body('last_name','first name cant be empty').notEmpty(),
-],(req,res)=>{
-    const user = users.find(u=>u.id == req.params.id);
-    if (!user){
-        return res.status(404).json({
-            data : null,
-            message:'the user with given id was not found'
-        })
-    }
 
-   const errors = validationResult(req);
-   if (!errors.isEmpty()){
-    return res.status(400).json({data:null,errors:errors.array(),message:'validation error'})
-   }
-   users = users.map(user=>{
-   if (user.id == req.params.id){
-    return {...user,...req.body}
-   }
-   return user
-   })
+app.use('/api/users', userRouter )
+app.use('/', homeRouter )
 
-res.json({
-    data : users,
-    message:'ok'
-})
-})
-app.delete('/api/users/:id',(req,res)=>{
-    const user = users.find(u=>u.id == req.params.id);
-    if (!user){
-        return res.status(404).json({
-            data : null,
-            message:'the user with given id was not found'
-        })
-    }
-
-  const index = users.indexOf(user);
-  users.splice(index,1)
-  res.json({
-    data : users,
-    message:'ok'
-})
-})
 
 const port =process.env.PORT || 3000
 app.listen(3000,()=>{
